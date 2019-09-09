4191fb8804c48223c0b5f692dc3296648097bbce
Michael Dunphy <michaeldunphy52@gmail.com>
2019-09-09 20:38:51 +0000

    mobile meta tag

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,16 @@
+class BadWordValidator < ActiveModel::Validator
+  def validate(record)
+    Rails.logger.info "#{record.saying}"
+    if record.saying == "ass"
+      Rails.logger.info "I'm also here!"
+      record.errors[:base] << "You used a bad word!"
+      Rails.logger.info "I'm really here!"
+    end
+  end
+end
+
 class Quote < ApplicationRecord
   validates :saying, presence: true, length: { maximum: 140, minimum: 3}
   validates :author, presence: true, length: { maximum: 50, minimum: 3}
+  validates_with BadWordValidator
 end
